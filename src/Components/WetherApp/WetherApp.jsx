dd0173a57ee0e2327c21ce8047419ec32b20e4ee
Tharindu <tharindu@Tharindus-MacBook-Air.local>
2023-10-11 07:33:58 +0530

    first commit

---
+++ src/Components/WetherApp/WetherApp.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import './WetherApp.css';
+import humidity_icon from  "../Assets/humidity.png";
+import clear_icon from  "../Assets/clear.png"
+import cloud_icon from  "../Assets/cloud.png";
+import drizzle_icon from  "../Assets/drizzle.png";
+import rain_icon from  "../Assets/rain.png";
+import search_icon from  "../Assets/search.png";
+import snow_icon from  "../Assets/snow.png";
+import wind_icon from  "../Assets/wind.png";
+
+
+const WetherApp = () => {
+  let api_key = "8548ee84b51708365ae2aa1689c3e13f";
+
+  const search = async ()=>
+  {
+    const element = document.getElementsByClassName("cityInput");
+    if(element[0].value==="")
+    {
+      return 0;
+    }
+    // let url = 'https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&unit=Metric&appid=${api_key}';
+  // let url = 'https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&unit=Metric&appid=8548ee84b51708365ae2aa1689c3e13f';
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=metric&appid=8548ee84b51708365ae2aa1689c3e13f`;
+  let response = await fetch(url);
+  let data =await response.json();
+
+  const humidity = document.getElementsByClassName("humidity-percent");
+  const wind=document.getElementsByClassName("wind-rate");
+  const temprature = document.getElementsByClassName("weather-temp");
+  const location=document.getElementsByClassName("weather-location");
+
+  humidity[0].innerHTML = data.main.humidity;
+  wind[0].innerHTML=data.wind.speed;
+  temprature[0].innerHTML= data.main.temp;
+  location[0].innerHTML = data.name;
+  }
+
+
+
+
+  return (
+    <div className='container'>
+      <div className='top-bar'>
+        <input type="text" className='cityInput' placeholder='search'>
+        </input>
+        <div className='search-icon'onClick={()=>{search()}}>
+            <img src={search_icon} alt=""></img>
+          </div>    
+      </div>
+         <div className='wether-image'>
+            <img src={cloud_icon} alt=""></img>
+          </div>
+          <div className='weather-temp'>24c</div>
+          <div className='weather-location'>Galle</div>
+          <div className='data-container'>
+          <div className='element'>
+            <img src={humidity_icon} alt="" className='icon'></img>
+              <div className='data'>
+                <div className='humidity-percent'>64%</div>
+                <div className='text'>Humidity</div>
+              </div>
+          </div>
+          <div className='element'>
+            <img src={wind_icon} alt="" className='icon'></img>
+              <div className='data'>
+                <div className='wind-rate'>18 km/h</div>
+                <div className='text'>Wind Speed</div>
+              </div>
+          </div>
+          </div>
+    </div>
+  )
+}
+
+export default WetherApp
